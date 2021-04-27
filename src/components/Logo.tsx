47349dd5d7a81a1c691fd968fb826531d1576b84
discosultan <jaanusvarus@gmail.com>
2021-04-27 19:31:34 +0200

    Bump deps; use different logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,24 +1,30 @@
-const styles = {
+import { CSSProperties } from 'react';
+
+const styles: { [key: string]: CSSProperties } = {
   logo: {
+    textAlign: "center",
+    fontSize: 64,
     margin: 0,
+    padding: "20px 0",
   },
 };
 
 export default function Logo() {
   return (
-    <pre style={styles.logo}>{`            |
-           / \\
-          / _ \\
-         |.o '.|
-         |'._.'|
-         |     |
-       .'|  |  |'.
-      /  |  |  |  \\
-      |.-'--|--'-.|
-     _                   
-    | |_   _ _ __   ___  
- _  | | | | | '_ \\ / _ \\ 
-| |_| | |_| | | | | (_) |
- \\___/ \\__,_|_| |_|\\___/`}</pre>
+    <p style={styles.logo}>🚀</p>
+//     <pre style={styles.logo}>{`            |
+//            / \\
+//           / _ \\
+//          |.o '.|
+//          |'._.'|
+//          |     |
+//        .'|  |  |'.
+//       /  |  |  |  \\
+//       |.-'--|--'-.|
+//      _                   
+//     | |_   _ _ __   ___  
+//  _  | | | | | '_ \\ / _ \\ 
+// | |_| | |_| | | | | (_) |
+//  \\___/ \\__,_|_| |_|\\___/`}</pre>
   );
 }
